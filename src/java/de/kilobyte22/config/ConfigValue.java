9b7d10e1b24e6c79f52c2f6b44a70b8cbb50c4b2
Kilobyte22 <stiepen22@gmx.de>
2015-01-03 23:07:04 +0100

    Fixed gradle file paths

--- src/java/de/kilobyte22/config/ConfigValue.java
+++
@@ -1,6 +0,0 @@
-package de.kilobyte22.config;
-
-public abstract class ConfigValue {
-    @Override
-    public abstract String toString();
-}
--- src/java/de/kilobyte22/config/ParserException.java
+++
@@ -1,7 +0,0 @@
-package de.kilobyte22.config;
-
-public class ParserException extends RuntimeException {
-    public ParserException(String message, Object... args) {
-        super(String.format(message, args));
-    }
-}
